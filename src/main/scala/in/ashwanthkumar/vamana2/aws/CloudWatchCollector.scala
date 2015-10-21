ab144c6389f520095c3c0db976a7623c592e5e2e
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-10-21 05:48:22 +0530

    Adding CloudWatchCollector implementation

---
+++ src/main/scala/in/ashwanthkumar/vamana2/aws/CloudWatchCollector.scala
@@ -0,0 +1,27 @@
+package in.ashwanthkumar.vamana2.aws
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest
+import in.ashwanthkumar.vamana2.core._
+
+import scala.collection.JavaConverters._
+
+class CloudWatchCollector(client: AmazonCloudWatchClient) extends Collector {
+  override def collectMetrics(namespace: String, metrics: List[String]): List[Metric] = {
+    metrics.map(metric => {
+      val result = client.getMetricStatistics(
+        new GetMetricStatisticsRequest()
+          .withNamespace(namespace)
+          .withMetricName(metric)
+      )
+      val points = result.getDatapoints.asScala
+        .map(datapoint => Point(datapoint.getSum, datapoint.getTimestamp.getTime))
+        .toList
+      Metric(result.getLabel, points)
+    })
+  }
+}
+
+object CloudWatchCollector {
+  def apply(): CloudWatchCollector = new CloudWatchCollector(new AmazonCloudWatchClient)
+}
