32984418a6592bfb0cf082d6768b5574b75ed02d
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-10-21 06:29:07 +0530

    AutoScalar abstraction + AWS implementation

---
+++ src/main/scala/in/ashwanthkumar/vamana2/aws/AutoScalingGroups.scala
@@ -0,0 +1,49 @@
+package in.ashwanthkumar.vamana2.aws
+
+import com.amazonaws.services.autoscaling.AmazonAutoScalingClient
+import com.amazonaws.services.autoscaling.model.{UpdateAutoScalingGroupRequest, DescribeAutoScalingGroupsRequest}
+import in.ashwanthkumar.vamana2.core.AutoScalar
+
+import scala.collection.JavaConverters._
+
+class AutoScalingGroups(client: AmazonAutoScalingClient) extends AutoScalar {
+  /**
+   * @inheritdoc
+   */
+  override def currentNodes(cluster: String): Int = {
+    val asgs = client.describeAutoScalingGroups(
+      new DescribeAutoScalingGroupsRequest()
+        .withAutoScalingGroupNames(cluster)
+    ).getAutoScalingGroups.asScala.toList
+
+    asgs match {
+      case x if x.isEmpty => throw new RuntimeException(s"Autoscaling Group $cluster not found")
+      case x if x.size > 1 => throw new RuntimeException(s"Multiple AutoScaling groups found for $cluster")
+      case x :: tail => x.getDesiredCapacity
+    }
+  }
+  /**
+   * @inheritdoc
+   */
+  override def supportsScaleTo: Boolean = true
+
+  /**
+   * @inheritdoc
+   */
+  override def scaleTo(cluster: String, totalNodes: Int): Unit = {
+    client.updateAutoScalingGroup(new UpdateAutoScalingGroupRequest()
+      .withAutoScalingGroupName(cluster)
+      .withDesiredCapacity(totalNodes)
+    )
+  }
+
+  /**
+   * @inheritdoc
+   */
+  override def scaleDown(cluster: String, nodes: Int): Unit = throw new RuntimeException("use scaleTo, I don't support scaleDown")
+
+  /**
+   * @inheritdoc
+   */
+  override def scaleUp(cluster: String, newNodes: Int): Unit = throw new RuntimeException("use scaleTo, I dont' support scaleUp")
+}
---
+++ src/main/scala/in/ashwanthkumar/vamana2/core/AutoScalar.scala
@@ -0,0 +1,43 @@
+package in.ashwanthkumar.vamana2.core
+
+trait AutoScalar {
+  /**
+   * Current number of nodes on the AutoScalar.
+   * <p />
+   * This number includes the desired the state that AutoScalar must satisfy, this need not match the *actual* number
+   * of instances / nodes behind the AutoScalar.
+   * <p />
+   * Example - In case of AWS AutoScaling this number represents the "Desired" value
+   */
+  def currentNodes(cluster: String): Int
+
+  /**
+   * AutoScalars like AWS AutoScaling Groups supports a value called desired which would automatically take care
+   * of scaling up / scaling down based on the totalNodes.
+   *
+   * AutoScalar#scaleTo is supposed to do that. For systems that doesn't support this
+   * AutoScalar#supportsScaleTo should return false
+   */
+  def scaleTo(cluster: String, totalNodes: Int)
+
+  /**
+   * Does the AutoScalar supports scaleTo?
+   *
+   * If the AutoScalar supports scaleTo we will not be using scaleUp / scaleDown methods.
+   *
+   * @see AutoScalar#scaleTo
+   */
+  def supportsScaleTo: Boolean
+
+  /**
+   * When AutoScalar doesn't support scaleTo, scaleUp is responsible for scaling up the cluster by <em>newNodes</em>.
+   * @param newNodes New number of nodes to scale up to
+   */
+  def scaleUp(cluster: String, newNodes: Int)
+
+  /**
+   * When AutoScalar doesn't support scaleTo, scaleDown is responsible for scaling down the cluster by <em>nodes</em>.
+   * @param nodes
+   */
+  def scaleDown(cluster: String, nodes: Int)
+}
