78cca54fcd4462c9fa61f9531c2aaaddafd05227
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-10-23 00:29:55 +0530

    fixing the tests as per the configuration

--- src/test/scala/in/ashwanthkumar/vamana2/core/ConfigReaderTest.scala
+++ src/test/scala/in/ashwanthkumar/vamana2/core/ConfigReaderTest.scala
@@ -9,23 +9,23 @@ class ConfigReaderTest extends FlatSpec {
     vamanaConfig.clusters should have size 1
 
     val cluster = vamanaConfig.clusters.head
-    cluster.name should be("Hadoop1 Staging Cluster")
-    cluster.asId should be("as-hadoop-staging-spot")
-    cluster.maxNodes should be(5)
+    cluster.name should be("Hadoop Datapipeline Cluster")
+    cluster.asId should be("as-datapipeline-tt-staging-spot")
+    cluster.maxNodes should be(20)
     cluster.minNodes should be(1)
     cluster.collector should be("in.ashwanthkumar.vamana2.aws.CloudWatchCollector")
     cluster.autoscalar should be("in.ashwanthkumar.vamana2.aws.AutoScalingGroups")
-    cluster.scalar should be("in.ashwanthkumar.vamana2.examples.HadoopScalar")
+    cluster.scalar should be("in.ashwanthkumar.vamana2.apps.HadoopScalar")
 
     val expectedMetrics = MetricsConfig(
-      demand = List("map_count_demand", "reduce_count_demand"),
-      supply = List("map_count_supply", "reduce_count_supply"),
-      namespace = Some("HadoopStaging"),
+      demand = List("map_demand", "reduce_demand"),
+      supply = List("map_supply", "reduce_supply"),
+      namespace = Some("HadoopAutoScaling"),
       dimensions = Map(
-        "name1" -> "value1",
-        "name2" -> "value2"
+        "Environment" -> "staging",
+        "Cluster" -> "datapipeline"
       ),
-      durationInMinutes = 10
+      durationInMinutes = 8 * 60 // 8 hours
     )
     cluster.metricsConfig should be(expectedMetrics)
   }
